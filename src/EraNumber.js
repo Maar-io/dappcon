3b9fac8ec894916116498649a42617d035b57e61
Mar.io <mario@stake.co.jp>
2021-10-06 15:25:04 +0200

    Current era removing warnings

--- src/EraNumber.js
+++ src/EraNumber.js
@@ -3,32 +3,28 @@ import { Statistic, Grid, Card, Icon } from 'semantic-ui-react';
 
 import { useSubstrate } from './substrate-lib';
 
-function Main(props) {
+function Main (props) {
   const { api } = useSubstrate();
   // const { network } = props;
   const [era, setCurrentEra] = useState(0);
   const [eraCountdown, setEraCountdown] = useState(0);
-  const [blocks, setBlocks] = useState(0);
-
   const blockPerEra = api.consts.dappsStaking.blockPerEra;
   const currentEra = api.query.dappsStaking.currentEra;
-  const bestNumber = api.derive.chain.bestNumber;
   const blockDuration = 2; // TODO duration per network
 
   useEffect(() => {
     let unsubscribeAll = null;
-    blockPerEra.isNone ? setBlocks('None') : setBlocks(blockPerEra.toString());
     currentEra(number => {
       setCurrentEra(number.toNumber());
-      setEraCountdown(blocks * blockDuration);
+      setEraCountdown(blockPerEra.toNumber() * blockDuration);
     })
       .then(unsub => {
         unsubscribeAll = unsub;
       })
       .catch(console.error);
 
-    console.log('blocks', blocks);
-    console.log('eraCountdown', eraCountdown);
+    console.log('currentEra', currentEra);
+    console.log('blockPerEra', blockPerEra);
     return () => unsubscribeAll && unsubscribeAll();
   }, [currentEra, blockPerEra]);
 
@@ -58,7 +54,7 @@ function Main(props) {
   );
 }
 
-export default function CurrentEra(props) {
+export default function CurrentEra (props) {
   const { api } = useSubstrate();
   return api.query.dappsStaking.currentEra
     ? <Main {...props} />

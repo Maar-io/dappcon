00c2d962e54ece293189fe768f09575863abf6d9
Mar.io <mario@stake.co.jp>
2021-10-07 18:57:47 +0200

    bad src/DappsCount.js

--- src/DappsCount.js
+++ src/DappsCount.js
@@ -1,33 +1,40 @@
-import { Null } from '@polkadot/types';
 import React, { useEffect, useState } from 'react';
 import { Statistic, Grid, Card } from 'semantic-ui-react';
 
 import { useSubstrate } from './substrate-lib';
+const CONTRACT = '0x0000000000000000000000000000000000000002';
 
 function Main (props) {
   const { api } = useSubstrate();
   const [dappsCount, setDappsCount] = useState(0);
   const [preApproval, setPreApproval] = useState('unknown');
 
-  const regDapp = api.query.dappsStaking.registeredDapps;
+  const getAddressEnum = (address) => (
+    [{"Evm": address}]
+  );
+
+  const regDapp = api.query.dappsStaking.registeredDevelopers;
   useEffect(() => {
     let unsubscribe;
     api.query.dappsStaking.preApprovalIsEnabled((result) => {
       setPreApproval(result.toString());
     });
-    console.log('registeredDapps', dappsCount);
-    regDapp(Null, newValue => {
-      if (newValue.isNone) {
+    console.log(getAddressEnum(CONTRACT));
+    regDapp({}, result => {
+      if (result.isNone) {
         setDappsCount('<None>');
-        console.log('newValue <None>');
+        console.log('registeredDapps <None>');
       } else {
-        setDappsCount(newValue.unwrap().toNumber());
-        console.log('newValue.unwrap()', newValue.unwrap());
+        // const tvl = result.unwrap().staked.valueOf() / DECIMALS;
+        // setStakedTotal(tvl);
+        setDappsCount(100);
+        console.log('registeredDapps.unwrap()', result.unwrap());
       }
     }).then(unsub => {
       unsubscribe = unsub;
     })
-      .catch(console.error);
+    .catch(console.error);
+    console.log('registeredDapps end', dappsCount);
 
     return () => unsubscribe && unsubscribe();
   }, [api.query.dappsStaking, dappsCount]);

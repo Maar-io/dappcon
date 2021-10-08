622185359be3f60cae5d739cae0d25a9409a16a3
Mar.io <mario@stake.co.jp>
2021-10-08 18:40:24 +0200

    dappsCount working

--- src/DappsCount.js
+++ src/DappsCount.js
@@ -2,54 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { Statistic, Grid, Card } from 'semantic-ui-react';
 
 import { useSubstrate } from './substrate-lib';
-const CONTRACT = '0x0000000000000000000000000000000000000002';
 
 function Main (props) {
   const { api } = useSubstrate();
   const [dappsCount, setDappsCount] = useState(0);
   const [preApproval, setPreApproval] = useState('unknown');
 
-  const getAddressEnum = (address) => (
-      {'Evm': address}
-    );
-
-  // const contractEraStake = api.query.dappsStaking.contractEraStake;
-  // useEffect(() => {
-  //   let unsubscribeAll = null;
-  //   contractEraStake(getAddressEnum(CONTRACT), 529, result => {
-  //     console.log('contractEraStake ', result.unwrap().toHuman());
-  //   })
-  //   .then(unsub => {
-  //     unsubscribeAll = unsub;
-  //   })
-  //   .catch(console.error);
-  //   return () => unsubscribeAll && unsubscribeAll();
-  // }, [contractEraStake]);
-
-
-  const regDapp = api.query.dappsStaking.registeredDapps;
   useEffect(() => {
     let unsubscribe;
     api.query.dappsStaking.preApprovalIsEnabled((result) => {
       setPreApproval(result.toString());
     });
-    console.log(getAddressEnum(CONTRACT));
-    regDapp(getAddressEnum(CONTRACT), result => {
-      if (result.isNone) {
-        setDappsCount('<None>');
-        console.log('registeredDapps <None>');
-      } else {
-        setDappsCount(100);
-        console.log('registeredDapps result', result.toString());
+
+    api.query.dappsStaking.registeredDapps.keys().then(
+      result => {
+        setDappsCount(result.length);
       }
-    }).then(unsub => {
-      unsubscribe = unsub;
-    })
-    .catch(console.error);
-    console.log('registeredDapps end', dappsCount);
+    )
+      .catch(console.error);
 
     return () => unsubscribe && unsubscribe();
-  }, [regDapp]);
+  }, [api.query.dappsStaking, api.query.dappsStaking.registeredDapps]);
 
   return (
     <Grid.Column>
@@ -70,7 +43,8 @@ function Main (props) {
 
 export default function DappsCount (props) {
   const { api } = useSubstrate();
-  return api.query.dappsStaking.registeredDapps
+  return api.query.dappsStaking &&
+    api.query.dappsStaking.registeredDapps
     ? <Main {...props} />
     : null;
 }
